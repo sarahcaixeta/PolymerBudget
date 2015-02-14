bba60a1a6b61a72db87c384e04bc85d4fb280e1d
Sarah Caixeta <caixeta.sarah@gmail.com>
2015-02-14 01:55:43 -0200

    Refactoring calculation

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ classAccordingToValidation = function(from, to) {
     return 'hidden';
   }
 },
-calculateBudget = function(from, to, amount){
+calculateBudget = function(from, to, amount, expenses){
   var today = moment();
   var initialDay = moment(from);
   var finalDay = moment(to);
@@ -15,13 +15,13 @@ calculateBudget = function(from, to, amount){
   } else {
     diffDays = finalDay.diff(today, 'days');
   }
-  return (amount / diffDays).toFixed(2);
+  var realbudget = amount - sumOfExpenses(expenses);
+  return (realbudget / diffDays).toFixed(2);
 },
 sumOfExpenses = function(expenses) {
   var total = 0;
   for (var i = 0; i < expenses.length; i++){
     total += Number(expenses[i].price);
   }
-  console.log(total);
   return total;
 }
